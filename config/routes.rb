16da10b0c4bbce7822b076689ca006dff4024765
Nicolás Órdenes <nicordenes@gmail.com>
2022-04-16 18:13:58 -0400

    Update for Heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
-Rails.application.routes.draw do
-  resources :cars
+#Rails.application.routes.draw do
+#  resources :cars
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+#end
+Rails.application.routes.draw do
+  root 'cars#index'
+  resources :cars
 end
